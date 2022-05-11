8b4e2564d33cd74d54e10d7567fb3728fa4cfed1
Roshan-Sen <roshandsen@gmail.com>
2022-05-10 22:32:35 -0700

    Updated 5/10, changed whole approach by simplifying the alignment process

--- sequencealignment.py
+++ sequencealignment.py
@@ -3,14 +3,6 @@ This library will contain methods for alignment of protein
 and DNA sequences. These will be utilized in another file.
 """
 
-#Makes a matrix
-def buildmatrix(height, width):
-	matrix = []
-	line = []
-	[line.append(0) for i in range(width)]
-	[matrix.append(line) for i in range(height)]
-	return matrix
-
 #Returns array of fragments from a string given
 #an array of index tuple pairs
 def extractsequences(sequence, indeces):
@@ -18,15 +10,31 @@ def extractsequences(sequence, indeces):
 	[seqarray.append(sequence[start:end]) for start, end in indeces]
 	return seqarray
 
+#Builds a matrix with the alignments, commonalities
+#are labelled with a 1.
+def alignmentmatrix(sequenceone, sequencetwo):
+	alignments = []
+	for charone in sequenceone:
+		line = []
+		for chartwo in sequencetwo:
+			if chartwo == charone: line.append(1)
+			else:                  line.append(0)
+		alignments.append(line)
+	return alignments
+
 #Function Tester
 def functiontester():
-	newmatrix = buildmatrix(10, 10)
-	print('A 10 X 10 matrix:', newmatrix)
-	samplesequenceone = 'ATGCTGATGTCATTTTGGTCTATCGTG'
-	samplespliceindeces = [(0, 6), (10, 15)]
-	print('Sample Sequence:', samplesequenceone)
-	print('What the splices should be:', samplesequenceone[0:6], samplesequenceone[10:15])
-	samplesplices = extractsequences(samplesequenceone, samplespliceindeces)
-	print('What the function extracted:', samplesplices)
+	print('Hello')
+	samplesequenceone = 'ACGTATAT'
+	samplesequencetwo = 'GCTACGT'
+	print('Testing alignment between ' + samplesequenceone + ' and ' + samplesequencetwo + '.')
+	samplealignmentmatrix = alignmentmatrix(samplesequenceone, samplesequencetwo)
+	print('  ', end = ' ')
+	for character in samplesequencetwo:
+		print(character, end = '  ')
+	print()
+	
+	for i in range(len(samplesequenceone)):
+		print(samplesequenceone[i], samplealignmentmatrix[i])
 
 functiontester()
